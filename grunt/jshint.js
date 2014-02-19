ce4452b322c177bd3284217b019164de79c307a2
Iszak Bryan <iszak@karmarama.com>
2014-02-19 18:50:24 +0000

    Lint grunt files

--- grunt/jshint.js
+++ grunt/jshint.js
@@ -1,6 +1,10 @@
-// JSHint JavaScript files
+// JSHint JS files
 module.exports = {
     app: [
         'js/app/*.js'
+    ],
+    grunt: [
+        'Gruntfile.js',
+        'grunt/*.js'
     ]
 };
