2cf68f5cd73a4d7d208e0d53b40d5e3901ae2cc5
Matine  Chabrier <matine.chabrier@karmarama.com>
2014-03-17 15:56:10 +0000

    Display output of grunt concurrent

--- grunt/concurrent.js
+++ grunt/concurrent.js
@@ -1,20 +1,23 @@
 // Run tasks concurrently
 module.exports = {
-    default: [
-        'js',
-        'css',
-        'img'
-    ],
+    default: {
+        tasks: ['js', 'css', 'img'],
+        options: {
+            logConcurrentOutput: true
+        }
+    },
 
-    js: [
-        'js:lib',
-        'js:ext',
-        'js:polyfill',
-        'js:app'
-    ],
+    js: {
+        tasks: ['js:lib', 'js:ext', 'js:polyfill', 'js:app'],
+        options: {
+            logConcurrentOutput: true
+        }
+    },
 
-    css: [
-        'css:app',
-        'css:lib'
-    ]
+    css: {
+        tasks: ['css:app', 'css:lib'],
+        options: {
+            logConcurrentOutput: true
+        }
+    }
 };
